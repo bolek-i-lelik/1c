0407b0d9a6d8fe2b880686d3c99cc90c062e598e
root <root@comp.domen>
2016-11-13 02:34:46 +0300

    index

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -30,25 +30,56 @@ AppAsset::register($this);
         <div class="span3 bs-docs-sidebar hidden-xs">
             <ul class="nav nav-list bs-docs-sidenav affix">
                 <li class = "logo"><img src="/img/logo.png"><br><img src="/img/ramicon.png"></li>
-                <li><img src="/img/borderMain.png"><a href="#dropdowns">горячие <br>предложения</a></li>
-                <li><img src="/img/borderMain.png"><a href="#buttonGroups"><i class="icon-chevron-right"></i>программное <br>обеспечение</a></li>
-                <li><img src="/img/borderMain.png"><a href="#buttonDropdowns"><i class="icon-chevron-right"></i>подписки</a></li>
-                <li><img src="/img/borderMain.png"><a href="#navs"><i class="icon-chevron-right"></i>услуги</a></li>
-                <li><img src="/img/borderMain.png"><a href="#navbar">наши партнеры</a></li>
-                <li><img src="/img/borderMain.png"><a href="#breadcrumbs">почему мы</a></li>
-                <li><img src="/img/borderMain.png"><a href="#pagination">сертификаты</a></li>
-                <li><img src="/img/borderMain.png"><a href="#labels-badges">преимущества</a></li>
-                <li><img src="/img/borderMain.png"><a href="#typography">команда</a></li>
-                <li><img src="/img/borderMain.png"><a href="#thumbnails">калькулятор</a></li>
-                <li><img src="/img/borderMain.png"><a href="#alerts">контакты</a><img src="/img/borderMain.png"></li>
+                <li><img src="/img/borderMain.png"><a href="#slider">горячие <br>предложения</a></li>
+                <li><img src="/img/borderMain.png"><a href="#po"><i class="icon-chevron-right"></i>программное <br>обеспечение</a></li>
+                <li><img src="/img/borderMain.png"><a href="#podpiski"><i class="icon-chevron-right"></i>подписки</a></li>
+                <li><img src="/img/borderMain.png"><a href="#uslugi"><i class="icon-chevron-right"></i>услуги</a></li>
+                <li><img src="/img/borderMain.png"><a href="#partnery">наши партнеры</a></li>
+                <li><img src="/img/borderMain.png"><a href="#my">почему мы</a></li>
+                <li><img src="/img/borderMain.png"><a href="#sertif">сертификаты</a></li>
+                <li><img src="/img/borderMain.png"><a href="#preim">преимущества</a></li>
+                <li><img src="/img/borderMain.png"><a href="#command">команда</a></li>
+                <li><img src="/img/borderMain.png"><a href="#calc">калькулятор</a></li>
+                <li><img src="/img/borderMain.png"><a href="#cont">контакты</a><img src="/img/borderMain.png"></li>
                 <li class="tel"><img src="/img/tel.png">  <p>8(812)</p><p class="colorText">336-48-30</p></li>
-                <li> <a href="#" class="btn-ZZ">заказать звонок</a></li>
+                <li> <a href="#" id="btn-ZZ">заказать звонок</a></li>
 
             </ul>
         </div>
-        <nav class="navbar navbar-default navbar-static-top visible-xs" role="navigation">
-            <span class="glyphicon glyphicon-th-list"></span> 
-        </nav>
+        <div class="visible-xs">
+            <nav class="navbar navbar-default" role="navigation">
+                <!--<div class="container">-->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand"><img src="/img/ramicon.png"></a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            
+                            <li><img src="/img/borderMain.png"><a href="#slider">горячие <br>предложения</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#po"><i class="icon-chevron-right"></i>программное <br>обеспечение</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#podpiski"><i class="icon-chevron-right"></i>подписки</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#uslugi"><i class="icon-chevron-right"></i>услуги</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#partnery">наши партнеры</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#my">почему мы</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#sertif">сертификаты</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#preim">преимущества</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#command">команда</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#calc">калькулятор</a></li>
+                            <li><img src="/img/borderMain.png"><a href="#cont">контакты</a><img src="/img/borderMain.png"></li>
+                            <li class="tel"><img src="/img/tel.png">  <p>8(812)</p><p class="colorText">336-48-30</p></li>
+                            <li> <a href="#" id="btn-ZZ">заказать звонок</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                <!--</div>-->
+            </nav>
+        </div>
 
 
         <div class="span9">
--- views/site/index.php
+++ views/site/index.php
@@ -6,49 +6,48 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
-    <div><h1>test</h1></div>
+    <div><a name="slider"><h2>Горячие предложения</h2></a>
+    	<img src="/img/slide.png">
+    </div>
+    <div><a  name="po"><h2>программное обеспечение</h2></a>
+		<img src="/img/po.png">
+    </div>
+
+	<div><a name="podpiski"><h2>подписки</h2></a>
+		<img src="/img/podpis.png">
+	</div>
+	<div><a  name="uslugi"><h2>услуги</h2></a>
+	<img src="/img/uslugi.png">
+	</div>
+	<div><a name="partnery"><h2>партнеры</h2></a>
+	<img src="/img/partnery.png">
+	</div>
+	<div><a name="my"><h2>почему мы</h2></a>
+	<img src="/img/my.png">
+	</div>
+	<div><a name="sertif"><h2>наши сертификаты</h2></a>
+	<img src="/img/sert.png">
+	</div>
+	<div><a name="preim"><h2>наши преимущества</h2></a>
+	<img src="/img/preim.png">
+	</div>
+	<div><a name="command"><h2>наша команда</h2></a>
+	<img src="/img/command.png">
+	</div>
+	<div>
+		<img src="/img/obrZ.png"><img src="/img/otz.png">
+	</div>
+
+	<div><a name="calc"><h2>калькулятор</h2></a>
+	<img src="/img/calc.png">
+	</div>
+	<div>
+		<img src="/img/bp.png">
+	</div>
+	<div><a name="cont"><h2>контакты</h2></a>
+	<img src="/img/cont.png">
+	</div>
+	<div>
+		<img src="/img/map.png">
+	</div>
 </div>
