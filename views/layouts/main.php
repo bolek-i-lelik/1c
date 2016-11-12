e73ea2a3b49ba78ebbfe252a0cf14f95f493e426
root <root@comp.domen>
2016-11-13 00:55:42 +0300

    menuOut

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,24 +27,29 @@ AppAsset::register($this);
 
 <div class="container">
     <div class="row">
-        <div class="span3 bs-docs-sidebar">
+        <div class="span3 bs-docs-sidebar hidden-xs">
             <ul class="nav nav-list bs-docs-sidenav affix">
                 <li class = "logo"><img src="/img/logo.png"><br><img src="/img/ramicon.png"></li>
                 <li><img src="/img/borderMain.png"><a href="#dropdowns">горячие <br>предложения</a></li>
                 <li><img src="/img/borderMain.png"><a href="#buttonGroups"><i class="icon-chevron-right"></i>программное <br>обеспечение</a></li>
                 <li><img src="/img/borderMain.png"><a href="#buttonDropdowns"><i class="icon-chevron-right"></i>подписки</a></li>
                 <li><img src="/img/borderMain.png"><a href="#navs"><i class="icon-chevron-right"></i>услуги</a></li>
-                <li><img src="/img/borderRazd.png"><a href="#navbar">наши партнеры</a></li>
+                <li><img src="/img/borderMain.png"><a href="#navbar">наши партнеры</a></li>
                 <li><img src="/img/borderMain.png"><a href="#breadcrumbs">почему мы</a></li>
                 <li><img src="/img/borderMain.png"><a href="#pagination">сертификаты</a></li>
                 <li><img src="/img/borderMain.png"><a href="#labels-badges">преимущества</a></li>
                 <li><img src="/img/borderMain.png"><a href="#typography">команда</a></li>
-                <li><img src="/img/_xcf.png"><a href="#thumbnails">калькулятор</a></li>
+                <li><img src="/img/borderMain.png"><a href="#thumbnails">калькулятор</a></li>
                 <li><img src="/img/borderMain.png"><a href="#alerts">контакты</a><img src="/img/borderMain.png"></li>
                 <li class="tel"><img src="/img/tel.png">  <p>8(812)</p><p class="colorText">336-48-30</p></li>
+                <li> <a href="#" class="btn-ZZ">заказать звонок</a></li>
 
             </ul>
         </div>
+        <nav class="navbar navbar-default navbar-static-top visible-xs" role="navigation">
+            <span class="glyphicon glyphicon-th-list"></span> 
+        </nav>
+
 
         <div class="span9">
             <?= Breadcrumbs::widget([
