3091ac43b52e43f49780454ca9ae3c456eae9e93
seblex <burovav84@mail.ru>
2016-11-12 16:05:12 +0300

    backend

---
+++ models/Products.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property integer $id
+ * @property string $image
+ * @property string $link
+ * @property string $price
+ * @property string $description
+ * @property string $idjs
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['image', 'link', 'price', 'description'], 'required'],
+            [['image', 'link', 'price', 'description', 'idjs'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'image' => 'Image',
+            'link' => 'Link',
+            'price' => 'Price',
+            'description' => 'Description',
+            'idjs' => 'Idjs',
+        ];
+    }
+}
